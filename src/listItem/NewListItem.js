ebe41bb6097e64201a4c12a071e8fbaffc2f965e
lgernand@howardday.com <landongernand@outlook.com>
2022-12-16 17:49:47 -0600

    some styling

--- src/listItem/NewListItem.js
+++ src/listItem/NewListItem.js
@@ -3,16 +3,18 @@ import "./NewListItem.css"
 
 function NewListItem(props) {
     return(
-        <div className="NewListItem">
-            <input type="text" placeholder="enter your new task" 
-                onChange={props.handleUpdateNewItem}
-            ></input>
-            <input type="text" placeholder="how long do you think it will take?"
-                onChange={props.handleUpdateNewTime}
-            ></input>
-            <button
-                onClick={props.handleAddItem}
-            >post new task</button>
+        <div className="NewListItemContainer">
+            <div className="NewListItem">
+                <input type="text" placeholder="enter your new task" 
+                    onChange={props.handleUpdateNewItem}
+                ></input>
+                <input type="text" placeholder="how long do you think it will take?"
+                    onChange={props.handleUpdateNewTime}
+                ></input>
+                <button
+                    onClick={props.handleAddItem}
+                >post new task</button>
+            </div>
         </div>
     );
 }
